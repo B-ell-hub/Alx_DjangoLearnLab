9c303ea83c243d7c9c0335282d30ce2a72b7479e
B-ell-hub <isabellahbuyaki@gmail.com>
2025-11-24 11:16:03 +0300

    introduction to building api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # This is the key line your checker wants
 ]
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
