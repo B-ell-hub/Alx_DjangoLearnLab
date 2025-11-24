a1892259b24b7a7904a6e954307f55c5682bcc00
B-ell-hub <isabellahbuyaki@gmail.com>
2025-11-24 11:12:24 +0300

    introduction to building api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
-
-from django.urls import path
-from .views import BookList
+from django.urls import path, include  # Make sure 'include' is imported
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # This connects your API app URLs
+    path('api/', include('api.urls')),  # This is the key line your checker wants
 ]
-
-
