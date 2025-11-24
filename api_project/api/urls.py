d87758c1ad3d9681955f64c548078cac8472ba0c
B-ell-hub <isabellahbuyaki@gmail.com>
2025-11-24 11:10:00 +0300

    introduction to building api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
+
 from django.urls import path
 from .views import BookList
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-]
-urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # This connects your API app
+    path('api/', include('api.urls')),  # This connects your API app URLs
 ]
+
 
