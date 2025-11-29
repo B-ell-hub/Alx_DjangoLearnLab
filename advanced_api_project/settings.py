252ed777be96d7aad00fb5b0d0172e161e7b892d
B-ell-hub <isabellahbuyaki@gmail.com>
2025-11-29 22:18:12 +0300

    Rebuilt project correctly with serializers and models

--- advanced_api_project/settings.py
+++ advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)n6@pcm))kdsw$jlccmset^%o)7&7+g)kb=#k_mf)xqd^r7lr'
+SECRET_KEY = 'django-insecure-d4bbcm6fp)35e@s648uni31dmbja$s-4-*u%xsjge)#!x9k2h@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
