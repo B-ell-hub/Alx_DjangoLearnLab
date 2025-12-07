e896db268d49dd09c87e4d20f88f33fd5caf0c10
B-ell-hub <isabellahbuyaki@gmail.com>
2025-12-07 12:20:58 +0300

    django blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
