50621fa47d2ed9caa225b7d64c3f753a1ffb5c1d
B-ell-hub <isabellahbuyaki@gmail.com>
2025-12-14 12:01:45 +0300

    building and deploying a django api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,6 +3,13 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from .models import User
 
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -11,14 +18,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            password=validated_data['password']
         )
+        user.bio = validated_data.get('bio', '')
+        user.save()
         Token.objects.create(user=user)
         return user
+
 
 
 class LoginSerializer(serializers.Serializer):
