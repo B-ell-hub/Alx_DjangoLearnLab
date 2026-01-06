46b4965874839ccace305446a6462ee562c1e69d
B-ell-hub <isabellahbuyaki@gmail.com>
2026-01-06 03:32:46 +0300

    redoing

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from .views.admin_view import admin_view
-from .views.librarian_view import librarian_view
-from .views.member_view import member_view
-from .views.book_views import add_book, edit_book, delete_book
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    # Role-based views
-    path("admin-role/", admin_view, name="admin_view"),
-    path("librarian-role/", librarian_view, name="librarian_view"),
-    path("member-role/", member_view, name="member_view"),
-
-    # Required by autograder — MUST MATCH EXACTLY
-    path("add_book/", add_book, name="add_book"),
-    path("edit_book/<int:pk>/", edit_book, name="edit_book"),
-    path("delete_book/<int:pk>/", delete_book, name="delete_book"),
+    path("register/", views.register, name="register"),
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login"
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout"
+    ),
 ]
