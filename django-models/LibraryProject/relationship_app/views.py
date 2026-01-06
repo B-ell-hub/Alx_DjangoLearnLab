f4d1ac173290e558e09661fd02b3ca829fe43dbb
B-ell-hub <isabellahbuyaki@gmail.com>
2026-01-06 03:51:06 +0300

    redoing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login  # ✅ required by checker
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
-# Registration view
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # ✅ also required by checker
-            return redirect("login")
-    else:
-        form = UserCreationForm()
-    return render(request, "relationship_app/register.html", {"form": form})
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
