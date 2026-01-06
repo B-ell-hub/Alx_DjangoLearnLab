c6620c0e3ec9b9a4881578bdf87059fbb7ecb996
B-ell-hub <isabellahbuyaki@gmail.com>
2026-01-06 03:28:46 +0300

    redoing

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,44 +1,31 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
 
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        return self.create_user(username, email, password, **extra_fields)
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
-
-    objects = CustomUserManager()
+class Author(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.username
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey("Author", on_delete=models.CASCADE)
-    publication_year = models.PositiveIntegerField()
-    library = models.ForeignKey("Library", on_delete=models.CASCADE, related_name="books")
+        return self.name
 
-    # -----------------------
-    # Custom permissions
-    # -----------------------
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add a book"),
-            ("can_change_book", "Can change a book"),
-            ("can_delete_book", "Can delete a book"),
-        ]
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
